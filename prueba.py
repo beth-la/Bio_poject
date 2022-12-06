9560b78e9fc5d84a9a91d68d5195fa3c2e9fcb9b
Diana <diana.delgado.gtz@gmail.com>
2022-12-05 18:11:22 -0600

    prueba

--- prueba.py
+++ prueba.py
@@ -1,6 +1,6 @@
 # GEO analyzer
-<<<<<<< HEAD
+<< << << < HEAD
 # Soy Victor
-=======
-# Hola mundo
->>>>>>> 58fe3774cff577642f16f4082116feb7107318db
+== == == =
+# Hola mundo, estoy hasta la madre
+>>>>>> > 58fe3774cff577642f16f4082116feb7107318db
