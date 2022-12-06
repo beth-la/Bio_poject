9589747c8f2382dbe45e62871994da4f56be880e
Brenda <brenda.titanium@hotmail.com>
2022-12-05 21:06:09 -0600

    añadiendo funcion principal del proyecto

---
+++ src/Geo_analyzer.py
@@ -0,0 +1,47 @@
+'''
+Name
+    Geo_analyzer.py
+    
+Version
+    1.0
+    
+Author 
+    Lopez A. Brenda E.
+    
+Descripcion
+    
+Category
+    Biopython
+    
+Usage
+    Python Geo_analyzer.py [-h] -o ORGANISM -f FEATURE
+    
+Arguments
+    -h --help
+
+See also
+    None
+'''
+
+# Importando modulos:
+from entrez_module import entrez_query
+from entrez_module import format_gse
+import argparse
+
+# Argumentos
+arg_parser = argparse.ArgumentParser(description="")
+
+arg_parser.add_argument("-o", "--ORGANISM",
+                    help="Organismo para el cual se desea encontrar informacaion",
+                    required=True)
+                    
+arg_parser.add_argument("-f", "--FEATURE",
+                    help="Caracteristica asociada al organismo",
+                    required=True)            
+arguments = arg_parser.parse_args()
+
+# Obteniendo el query con la funcion entrez_query.
+query= entrez_query(arguments.ORGANISM, arguments.FEATURE)
+
+# Obteniendo los Ids de GSE asociados al query. 
+gse_ids = format_gse(query)
