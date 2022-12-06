a5b9f3f5c0510cbce3b6729e670b880d89f0865f
Brenda <brenda.titanium@hotmail.com>
2022-12-05 20:44:22 -0600

    añadiendo encabezado

--- src/entrez_module.py
+++ src/entrez_module.py
@@ -1,11 +1,47 @@
+'''
+MODULE NAME:
+    Entrez module
+
+DESCRIPTION:
+    Modulo que contiene funciones que nos permiten trabajar con Entrez.
+
+VERSION:
+    1.0
+    
+AUTHORS
+    Delgado Gutierrez Diana 
+    Lopez Angeles Brenda E.
+    Plasencia Perez Victor Ulises
+    
+Functions
+    entrez_query
+    format_gse
+    
+'''
+# Librerias utilizadas por el modulo
 from Bio import Entrez
 import re 
 
 def entrez_query(organism, feature):
+    '''
+    Genera un query que pueda ser utilizado con Entrez.
+    Parameters:
+        organism (str): Organismo para el cual se desea generar el query de busqueda.
+        feature (str): Caracteristica que acompaña al organismo buscado 
+    Returns:
+        query (str): Query generado. 
+    '''
     query= f"({feature}) AND ({organism} [Organism]) AND (Expression profiling by array [DataSet Type])" 
     return(query)
 
 def format_gse(query):
+    '''
+    Obtiene una lista con los IDs GSE de la base de datos GEO.
+    Parameters:
+        query (str): Query para el cual se desea realizar la busqueda.
+    Returns:
+        gse_list (list): Lista de IDs listos para ser procesados por GEOparse. 
+    '''
     Entrez.email = "blopez@lcg.unam.mx"
     handle = Entrez.esearch(db='gds', term=query, retmax=5, usehistory=True)
     result = Entrez.read(handle)
